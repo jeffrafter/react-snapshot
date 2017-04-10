c27d68019c7dda4cac6f18022d5c5c1c8739b756
jeffrafter <jeffrafter@gmail.com>
2017-04-10 10:18:59 -0700

    Try to respect the static Helmet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom';
 import ReactDOMServer from 'react-dom/server';
+import Helmet from 'react-helmet';
 
 export const render = (rootComponent, domElement) => {
   if (navigator.userAgent.match(/Node\.js/i)) {
     domElement.innerHTML = ReactDOMServer.renderToString(rootComponent)
+    Helmet.renderStatic()
   } else {
     ReactDOM.render(rootComponent, domElement)
   }
